580229461a976b9d141687da15ad66fc916b2d6f
Hector <hectoremil21@hotmail.com>
2020-05-22 12:00:49 -0400

    Create Pokemon object files

---
+++ Pokedex/Pokedex/Pokemon.h
@@ -0,0 +1,17 @@
+//
+//  Pokemon.h
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 5/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Pokemon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/Pokemon.m
@@ -0,0 +1,13 @@
+//
+//  Pokemon.m
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 5/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import "Pokemon.h"
+
+@implementation Pokemon
+
+@end
