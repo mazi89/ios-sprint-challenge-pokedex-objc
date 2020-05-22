c4b5fd4d556b11380c67aef2383b60680a043165
Hector <hectoremil21@hotmail.com>
2020-05-22 12:08:10 -0400

    Pokemon.m initializer

--- Pokedex/Pokedex/Pokemon.m
+++ Pokedex/Pokedex/Pokemon.m
@@ -10,4 +10,15 @@
 
 @implementation Pokemon
 
+- (instancetype) initWithName:(NSString *)name iD:(int)iD abilities:(NSArray<NSString *> *)abilities {
+    self = [super init];
+    if (self) {
+        _name = [name copy];
+        _iD = iD;
+        // MARK:- QUESTION FOR JON: Given that an NSMutableArray and an NSArray are two entirely different objects, I shouldn't worry about passing this by copy right?
+        _abilities = abilities;
+    }
+    return self;
+}
+
 @end
