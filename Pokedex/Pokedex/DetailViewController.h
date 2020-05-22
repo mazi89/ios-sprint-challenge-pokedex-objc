b7c5da6578598bcebc99977c55bbc5650fcd100c
Hector <hectoremil21@hotmail.com>
2020-05-22 12:10:44 -0400

    Table and Detail view controller files

---
+++ Pokedex/Pokedex/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 5/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/PokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokedexTableViewController.h
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 5/22/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
